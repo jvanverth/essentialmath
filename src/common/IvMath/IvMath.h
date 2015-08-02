66c9b138806f9fb56cc1cc544effca9b3aa784dc
Jim Van Verth <jvanverth@google.com>
2015-08-02 17:57:28 -0400

    Fix up base math functions

--- src/common/IvMath/IvMath.h
+++ src/common/IvMath/IvMath.h
@@ -32,61 +32,7 @@
 #define kHalfPI     1.5707963267948966192313216916398f
 #define kTwoPI      2.0f*kPI
 
-#if defined(__APPLE__) && defined(__MACH__)
-#define sqrtf sqrt
-#define sinf sin
-#define cosf cos
-#define fabsf fabs
-#define tanf tan
-#define atan2f atan2
-#define acosf acos
-#endif
-
 #undef IV_APPROXIMATION   // commented out for the moment
-typedef union {float f; int i;} IntOrFloat;
-
-//
-// This routine is from Lomont, "Floating Point Tricks," _Game Gems 6_
-// See also www.lomont.org
-//
-inline float IvSqrt( float val )
-{ 
-#if defined(IV_APPROXIMATION)
-      assert(val >= 0);
-      IntOrFloat workval;
-      workval.f = val;
-      workval.i -= 0x3f800000; // subtract 127 from biased exponent
-      workval.i >>= 1;         // requires signed shift to preserve sign
-      workval.i += 0x3f800000; // rebias the new exponent
-      workval.i &= 0x7FFFFFFF; // remove sign bit
-      return workval.f;
-#else
-      return sqrtf( val ); 
-#endif
-}
-
-//
-// This routine is a classic approximation to rsqrt, attributed to
-// Greg Walsh.
-// The optimized values used are from http://rrrola.wz.cz/inv_sqrt.html
-//
-inline float IvRecipSqrt( float val )
-{ 
-#if defined(IV_APPROXIMATION)
-    IntOrFloat workval;
-    workval.f = val;
-    // initial guess y0 with magic number
-    workval.i = 0x5F1FFFF9 - (workval.i>>1);
-    // Newton step, increases accuracy
-    const float estimate_sq = workval.f*workval.f;
-    workval.f *= 0.703952253f*(2.38924456f-val*estimate_sq);
-    return workval.f;
-#else
-    return 1.0f/sqrtf( val );
-#endif
-} // RecipSqrt
-
-inline float IvAbs( float f )           { return fabsf(f); }
 
 //-------------------------------------------------------------------------------
 //-- Classes --------------------------------------------------------------------
@@ -100,6 +46,67 @@ extern void IvFastSinCos( float a, float& sina, float& cosa );
 //-------------------------------------------------------------------------------
 //-- Inlines --------------------------------------------------------------------
 //-------------------------------------------------------------------------------
+
+typedef union {float f; int i;} IntOrFloat;
+
+//-------------------------------------------------------------------------------
+// @ IvSqrt()
+//-------------------------------------------------------------------------------
+// Return square root of x, or close approximation
+//
+// This routine is from Lomont, "Floating Point Tricks," _Game Gems 6_
+// See also www.lomont.org
+//-------------------------------------------------------------------------------
+inline float IvSqrt( float val )
+{
+#if defined(IV_APPROXIMATION)
+    assert(val >= 0);
+    IntOrFloat workval;
+    workval.f = val;
+    workval.i -= 0x3f800000; // subtract 127 from biased exponent
+    workval.i >>= 1;         // requires signed shift to preserve sign
+    workval.i += 0x3f800000; // rebias the new exponent
+    workval.i &= 0x7FFFFFFF; // remove sign bit
+    return workval.f;
+#else
+    return sqrtf( val );
+#endif
+}
+
+
+//-------------------------------------------------------------------------------
+// @ IvRecipSqrt()
+//-------------------------------------------------------------------------------
+// Return 1/sqrt(x), or close approximation
+//
+// This routine is a classic approximation to rsqrt, attributed to
+// Greg Walsh.
+// The optimized values used are from http://rrrola.wz.cz/inv_sqrt.html
+//-------------------------------------------------------------------------------
+inline float IvRecipSqrt( float val )
+{
+#if defined(IV_APPROXIMATION)
+    IntOrFloat workval;
+    workval.f = val;
+    // initial guess y0 with magic number
+    workval.i = 0x5F1FFFF9 - (workval.i>>1);
+    // Newton step, increases accuracy
+    const float estimate_sq = workval.f*workval.f;
+    workval.f *= 0.703952253f*(2.38924456f-val*estimate_sq);
+    return workval.f;
+#else
+    return 1.0f/sqrtf( val );
+#endif
+}
+
+
+//-------------------------------------------------------------------------------
+// @ IvAbs()
+//-------------------------------------------------------------------------------
+// Absolute value
+//-------------------------------------------------------------------------------
+inline float IvAbs( float f )           { return fabsf(f); }
+
 
 //-------------------------------------------------------------------------------
 // @ IvIsZero()
@@ -167,11 +174,36 @@ inline float IvTan( float a )
 //-------------------------------------------------------------------------------
 inline void IvSinCos( float a, float& sina, float& cosa )
 {
+#if defined(IV_APPROXIMATION)
+    IvFastSinCos(a, sina, cosa);
+#else
     sina = sinf(a);
     cosa = cosf(a);
+#endif
 
 }  // End of IvSinCos
 
+
+//-------------------------------------------------------------------------------
+// @ IvIsInfinite()
+//-------------------------------------------------------------------------------
+// Returns whether the value is infinite
+//-------------------------------------------------------------------------------
+inline bool IvIsInfinite( float x )
+{
+    return isinf(x);
+}
+
+
+//-------------------------------------------------------------------------------
+// @ IvIsNaN()
+//-------------------------------------------------------------------------------
+// Returns whether the value is not-a-number
+//-------------------------------------------------------------------------------
+inline bool IvIsNaN( float x )
+{
+    return isnan(x);
+}
 
 //-------------------------------------------------------------------------------
 //-- Externs --------------------------------------------------------------------
