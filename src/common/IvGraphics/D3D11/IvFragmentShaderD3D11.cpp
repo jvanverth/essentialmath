73574d29f8d2930eef927f8498be3c0c14ec1151
Jim Van Verth <jim.van.verth@gmail.com>
2015-08-02 11:18:11 -0400

    Missed some copyright info

--- src/common/IvGraphics/D3D11/IvFragmentShaderD3D11.cpp
+++ src/common/IvGraphics/D3D11/IvFragmentShaderD3D11.cpp
@@ -3,7 +3,11 @@
 // 
 // Description
 // ------------------------------------------------------------------------------
-// Copyright (C) 2008  Elsevier, Inc.
+// Copyright (C) 2008-2015  James M. Van Verth and Lars M. Bishop.
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 //
 // Implementation notes
 //===============================================================================
